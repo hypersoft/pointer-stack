3d72578ca56ddef68d42d69f01720c334aeb76e3
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-01-14 10:35:42 -0600

    Synchronized header with current declarations.

--- src/pointer-stack.h
+++ src/pointer-stack.h
@@ -25,9 +25,58 @@ typedef void   ( * PointerStackDeallocator ) ( void *        );
 /* API Procedures */
 
 // Returns a string to the license data distributed with this library originally.
-const char * pointer_stack_license(void);
+extern const char * pointer_stack_license(void);
 
-void pointer_stack_allocation (
+extern void pointer_stack_allocation (
 	PointerStackAllocator, PointerStackAllocator, PointerStackDeallocator
 );
+
+/* Retrieve and clear the error value associated with a PointerStack */
+extern unsigned long pointer_stack_error(PointerStack);
+
+/* Setup a dual buffering scheme on a PointerStack */
+extern void pointer_stack_optimize(PointerStack, unsigned long);
+
+/* Setup PointerStack's preallocation buffer */
+extern void pointer_stack_buffer(PointerStack, unsigned long value);
+
+/* Setup PointerStack's deallocation buffer */
+extern void pointer_stack_auto_pack(PointerStack, unsigned long value);
+
+/* deallocate storage */
+extern void pointer_stack_free(void);
+
+/* control index inversion on a PointerStack */
+extern void pointer_stack_invert(PointerStack, bool);
+
+/* decrement the reference counted lock on a PointerStack */
+extern bool pointer_stack_unlock(PointerStack);
+
+/* increment the reference counted lock on a PointerStack */
+extern bool pointer_stack_lock(PointerStack);
+
+/* get the count of elements in a PointerStack */
+extern unsigned long pointer_stack_get_count(PointerStack);
+
+/* get the private data associated with a PointerStack */
+extern void * pointer_stack_get_private(PointerStack);
+
+/* set the private data associated with a PointerStack */
+extern void pointer_stack_set_private(PointerStack, void *);
+
+/* get the data limit associated with a PointerStack */
+extern unsigned long pointer_stack_get_limit(PointerStack);
+
+/* set the private data associated with a PointerStack */
+void pointer_stack_set_limit(PointerStack, unsigned long);
+
+/* get the pointer to the pointer of an element in a PointerStack */
+extern void * pointer_stack_pointer(PointerStack, unsigned long);
+
+/* swap a value in a PointerStack out with new data */
+extern void * pointer_stack_poke(PointerStack, unsigned long, void *);
+
+/* Examine a value in a PointerStack */
+extern void * pointer_stack_peek(PointerStack, unsigned long);
+
 
