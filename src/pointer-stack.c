7bc63f965e4191af20ac56ca7cc1a6fa647e2519
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-01-14 10:12:11 -0600

    Added pointer_stack_set_private

--- src/pointer-stack.c
+++ src/pointer-stack.c
@@ -109,6 +109,11 @@ void * pointer_stack_get_private(PointerStack * stack) {
 	return stack->private;
 }
 
+/* set the private data associated with a PointerStack */
+void pointer_stack_set_private(PointerStack * stack, void * private) {
+	stack->private = private;
+}
+
 /* This is not a thread safe operation. Avoid use wherever possible */
 void pointer_stack_initialize(PointerStackAllocator create, PointerStackAllocator resize, PointerStackDeallocator destroy) {
 
