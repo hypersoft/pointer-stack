5571d0d05a9ac2641f20618d816d44b19c232e8f
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-01-16 19:34:40 -0600

    Added export
    pseudo code (untested)
    
    Modified: src/pointer-stack-io.c

--- src/pointer-stack-io.c
+++ src/pointer-stack-io.c
@@ -29,3 +29,13 @@ bool pointer_stack_free(PointerStackExport * block) {
 	} return false;
 }
 
+PointerStackExport * pointer_stack_export(PointerStack * stack, size_t from, size_t to) {
+	if ( ! HavePointerStack || ! HavePointerStackData ) return NULL;
+	if ( from >= stack->index || to >= stack->index ) return NULL;
+	size_t units = (to - from);
+	void * export = pointer_stack_lease((1 + units) * sizeof(void *));
+	memcpy(export, stack->item + (from * sizeof(void *)), units * sizeof(void *));
+	(void **) export[units * sizeof(void *)] = NULL;
+	return export;
+}
+
