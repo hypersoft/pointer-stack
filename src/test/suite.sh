fded6fc97be0f07f04a2dbc9d5d9a23a2a625147
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-01-20 17:48:16 -0600

    Added micro application test suite
    This is a bash script that reads a label for the test from stdin, then reads a line of shell code to test for success/fail. The process is repeated for each test.
    
    This is an advanced shell script, as each shell code is sourced into the shell, rather than evaluated.
    
    Generally, all you need to do is write your test application, and pipe the prescribed format of data into the shell script.
    
    The exit status reflects how many tests failed.
    
    Added: src/test/suite.sh

---
+++ src/test/suite.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Micro Application Test Suite
+
+# Copyright 2013, Triston J. Taylor, All rights reserved
+
+# To use this script, simply pipe in a label for your test, and issue a shell statement
+# that should succeed for each test.
+
+declare -i succeed=0 fail=0 count=0;
+
+while read label; do {
+	let count++;
+	read code;
+	source <(echo $code) || {
+		echo test case $label: failed;
+		let failed++;
+	} && {
+		echo test case $label: succeeded;
+		let succeed++;
+	}
+}
+
+echo '';
+echo $success/$count succeeded.
+echo $failed/$count failed.
+echo '';
+
+exit $failed;
+
