543c30a4311017d2cb04b3b9067abe07d5109cf1
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-01-20 03:04:46 -0600

    So far so good...
    Will have to reconvene tomorrow.... Don't want to wear myself too thin. Peace!
    
    Modified: src/test/ps-test.c

--- src/test/ps-test.c
+++ src/test/ps-test.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
 #include "../include/pointer-stack.h"
 
 int main() {
 	PointerStack test = pointer_stack_create();
+	pointer_stack_push(test, (void *) 12);
+	printf("%i\n", pointer_stack_pop(test));
 	return 0;
 }
