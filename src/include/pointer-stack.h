017cf37793baacd958050d6be758efabbbbc5f9a
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-01-19 22:45:17 -0600

    Added 'pointer_stack_get_slots' procedure to I/O group
    The procedure returns the number of free slots in a PointerStack

--- src/include/pointer-stack.h
+++ src/include/pointer-stack.h
@@ -63,6 +63,7 @@
 	
 	/* I/O */
 	extern size_t pointer_stack_get_count(PointerStack);
+	extern size_t pointer_stack_get_slots(PointerStack);
 	extern bool   pointer_stack_free(PointerStackExport);
 	extern PointerStackExport * pointer_stack_export(PointerStack, size_t, size_t);
 
--- src/pointer-stack-io.c
+++ src/pointer-stack-io.c
@@ -5,6 +5,7 @@
 /* Provides API:
 
 	extern size_t pointer_stack_get_count(PointerStack);
+	extern size_t pointer_stack_get_slots(PointerStack);
 	extern bool   pointer_stack_free(PointerStackExport);
 
 	extern PointerStackExport * pointer_stack_export(PointerStack, size_t, size_t);
@@ -20,6 +21,12 @@ typedef void PointerStackExport;
 /* Get the count of elements in a PointerStack */
 size_t pointer_stack_get_count(PointerStack * stack) {
 	if (HavePointerStack && HavePointerStackData) return stack->index;
+	return 0;
+}
+
+/* Get the count of free slots in a PointerStack */
+size_t pointer_stack_get_slots(PointerStack * stack) {
+	if (HavePointerStack && HavePointerStackData) return (stack->units - stack->index);
 	return 0;
 }
 
