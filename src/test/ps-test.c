029922648f4e0589efb05ac184ba67a7c0403f32
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-01-20 20:38:40 -0600

    New internal test framework
    Added 'write_test' puts label /sprintf -> buffer -> format+args/puts buffer wrapper
    
    Added first test case 'Create PointerStack'
    
    Modified: src/test/ps-test.c

--- src/test/ps-test.c
+++ src/test/ps-test.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
+#include <stdarg.h>
+
 #include "../include/pointer-stack.h"
 
+char buffer[1024];
+
+void write_test(char * label, char * format, ...) {
+
+	puts(label);
+
+    va_list args; va_start(args, format);
+	vsnprintf(buffer, 1024, format, args);
+
+	puts(buffer);
+
+}
+
+void test_create_pointer_stack() {
+	PointerStack stack = pointer_stack_create();
+	write_test("Create PointerStack", "[[ %li != 0 ]]", (long int) stack);
+	pointer_stack_dispose(stack);
+}
+
 int main() {
-	PointerStack test = pointer_stack_create();
-	pointer_stack_push(test, (void *) 12);
-	printf("%i\n", pointer_stack_pop(test));
+	test_create_pointer_stack();
 	return 0;
 }
+
